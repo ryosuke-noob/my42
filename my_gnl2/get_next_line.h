91b558856c936c3a89f3af45c7b5946fb7c98778
ryosuke <nutar@HuaweinoMacBook-Pro.local>
2023-02-05 21:12:35 +0900

    apply norm

--- my_gnl2/get_next_line.h
+++ my_gnl2/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: nutar <nutar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/23 23:19:10 by nutar             #+#    #+#             */
-/*   Updated: 2022/10/30 23:20:00 by nutar            ###   ########.fr       */
+/*   Updated: 2023/02/05 21:10:59 by nutar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdio.h>
-
-// # define BUFFER_SIZE 5
 
 char	*get_next_line(int fd);
 char	*ft_strjoin_edit(char *s1, char *s2);
--- my_gnl2/get_next_line_bonus.h
+++ my_gnl2/get_next_line_bonus.h
@@ -1,23 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nutar <nutar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/23 23:19:10 by nutar             #+#    #+#             */
-/*   Updated: 2022/10/30 23:20:00 by nutar            ###   ########.fr       */
+/*   Updated: 2023/02/05 21:11:58 by nutar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdio.h>
-
-// # define BUFFER_SIZE 5
 
 char	*get_next_line(int fd);
 char	*ft_strjoin_edit(char *s1, char *s2);
