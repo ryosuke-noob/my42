88f11eda1d13bc70e4e8bb9e257e3c7853dd55c8
ryosuke <nutar@HuaweinoMacBook-Pro.local>
2023-03-24 22:04:23 +0900

    push check argv

--- my_pushswap/main.c
+++ my_pushswap/main.c
@@ -6,15 +6,14 @@
 /*   By: nutar <nutar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 18:44:56 by nutar             #+#    #+#             */
-/*   Updated: 2023/03/24 21:55:12 by nutar            ###   ########.fr       */
+/*   Updated: 2023/03/24 22:03:33 by nutar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int argc, char **argv)
+static t_integers	arg_check(int argc, char **argv)
 {
-	int			i;
 	t_integers	integers;
 
 	if (argc < 2)
@@ -23,6 +22,20 @@ int	main(int argc, char **argv)
 		integers = arg_2_check_all(argc, argv);
 	else
 		integers = arg_check_all(argc, argv);
+	return (integers);
+}
+
+int	main(int argc, char **argv)
+{
+	int			i;
+	t_integers	integers;
+
+	integers = arg_check(argc, argv);
+
+
+
+
+	//check
 	i = -1;
 	while (++i < integers.argc)
 		printf("%d\n", integers.array[i]);
