5dda835ad2b0c15c131c1a592c78b3c0c43df73d
ryosuke <nutar@HuaweinoMacBook-Pro.local>
2023-03-30 18:21:36 +0900

    make branch size==4

--- my_pushswap/sort.c
+++ my_pushswap/sort.c
@@ -6,7 +6,7 @@
 /*   By: nutar <nutar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 21:33:23 by nutar             #+#    #+#             */
-/*   Updated: 2023/03/30 16:39:40 by nutar            ###   ########.fr       */
+/*   Updated: 2023/03/30 18:16:06 by nutar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,12 +68,55 @@ static void	b_sort(t_stack *stack, int size, int max, int min)
 {
 	int	i;
 
+	// printf("[size==%d, max==%d]\n",size,max);
+	// check_stack(stack);
 	if (size <= 3)
 	{
 		asc_sort(stack, 'b');
 		i = -1;
 		while (++i < size)
 			push_back(stack);
+		return ;
+	}
+	if (size == 4)
+	{
+		// printf("[size==4, max==%d]\n",max);
+		i = -1;
+		while (++i < size)
+		{
+			if (stack->stack_b->number == max)
+				pa(stack);
+			else
+				rb(stack, 'b');
+			// check_stack(stack);
+		}
+		asc_sort(stack, 'b');
+		i = -1;
+		while (++i < 3)
+			push_back(stack);
+		ra(stack, 'a');
+		return ;
+	}
+	if (size == 5)
+	{
+		// printf("[size==4, max==%d]\n",max);
+		i = -1;
+		while (++i < size)
+		{
+			if (stack->stack_b->number == max || stack->stack_b->number == max - 1)
+				pa(stack);
+			else
+				rb(stack, 'b');
+			// check_stack(stack);
+		}
+		if (stack->stack_a->number > stack->stack_a->next->number)
+			sa(stack, 'a');
+		asc_sort(stack, 'b');
+		i = -1;
+		while (++i < 3)
+			push_back(stack);
+		ra(stack, 'a');
+		ra(stack, 'a');
 		return ;
 	}
 	i = -1;
@@ -84,7 +127,7 @@ static void	b_sort(t_stack *stack, int size, int max, int min)
 		else
 			rb(stack, 'b');
 	}
-	b_sort(stack, stack->size_b, max - size / 2, min);
+	b_sort(stack, stack->size_b, max - size / 2 - size % 2, min);
 	i = -1;
 	while (++i < size - size / 2)
 		pb(stack);
