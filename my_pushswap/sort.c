75137823554a7105e29367d2cec9f22e8fb22c40
ryosuke <nutar@HuaweinoMacBook-Pro.local>
2023-04-03 19:35:44 +0900

    add b_sort(/3) comment out

--- my_pushswap/sort.c
+++ my_pushswap/sort.c
@@ -6,7 +6,7 @@
 /*   By: nutar <nutar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 21:33:23 by nutar             #+#    #+#             */
-/*   Updated: 2023/04/02 23:05:26 by nutar            ###   ########.fr       */
+/*   Updated: 2023/04/03 19:34:15 by nutar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -108,6 +108,111 @@ int	count_last_max(t_stack *stack, int size, int max)
 	}
 	return (count);
 }
+// //3 div
+// static void	b_sort(t_stack *stack, int size, int max, int min)
+// {
+// 	int	i;
+// 	int	tmp_min;
+// 	// int	cnt_min;
+// 	int	cnt_max;
+
+// 	if (size <= 6)
+// 	{
+// 		// printf("\nsize <= 6 [size: %d, max: %d, min: %d]]", size, max, min);
+// 		// check_stack(stack);
+// 		cnt_max = 0;
+// 		while (stack->size_b)
+// 		{
+// 			if (stack->stack_b->number == min)
+// 			{
+// 				pa(stack);
+// 				min++;
+// 				if (stack->stack_b != NULL && stack->stack_b->number != max - cnt_max \
+// 						&& stack->stack_b->number != min && (check_r_rr(*stack, max - cnt_max) == 'r' || check_r_rr(*stack, min) == 'r'))
+// 					rr(stack);
+// 				else
+// 					ra(stack, 'a');
+// 			}
+// 			else if (stack->stack_b->number == max - cnt_max)
+// 			{
+// 				pa(stack);
+// 				cnt_max++;
+// 			}
+// 			else if (check_r_rr(*stack, max - cnt_max) == 'r' || check_r_rr(*stack, min) == 'r')
+// 				rb(stack, 'b');
+// 			else
+// 				rrb(stack, 'b');
+// 		}
+// 		i = -1;
+// 		while (++i < cnt_max)
+// 			ra(stack, 'a');
+// 		return ;
+// 	}
+// 	//top
+// 	// printf("\ntop [size: %d, max: %d, min: %d]]", size, max, min);
+// 	// check_stack(stack);
+// 	// cnt_min = count_min(stack, size, min, 'b');
+// 	tmp_min = min;
+// 	i = -1;
+// 	while (++i < size)
+// 	{
+// 		if (stack->stack_b->number == min)
+// 		{
+// 			pa(stack);
+// 			min++;
+// 			if (stack->stack_b != NULL && stack->stack_b->number != min \
+// 							&& stack->stack_b->number < size / 3 + tmp_min)
+// 			{
+// 				rr(stack);
+// 				i++;
+// 			}
+// 			else
+// 				ra(stack, 'a');
+// 		}
+// 		else if (stack->stack_b->number >= size / 3 + tmp_min)
+// 			pa(stack);
+// 		else
+// 			rb(stack, 'b');
+// 	}
+// 	b_sort(stack, stack->size_b, size / 3 + tmp_min - 1, min);
+// 	//middle
+// 	if (min <= size / 3 + tmp_min)
+// 		min = size / 3 + tmp_min;
+// 	// printf("\nmiddle [size: %d, max: %d, min: %d]]", size, max, min);
+// 	// check_stack(stack);
+// 	i = -1;
+// 	while (++i < size - size / 3)
+// 	{
+// 		if (stack->stack_a->number < size / 3 + min)
+// 			pb(stack);
+// 		else
+// 			ra(stack, 'a');
+// 	}
+// 	i = -1;
+// 	while (++i < size - size / 3 - size / 3)
+// 		rra(stack, 'a');
+// 	b_sort(stack, stack->size_b, size / 3 + min - 1, min);
+// 	//bottom
+// 	min = size / 3 + min;
+// 	// printf("\nbottom [size: %d, max: %d, min: %d]]", size, max, min);
+// 	// check_stack(stack);
+// 	cnt_max = count_last_max(stack, max - min + 1, max);
+// 	tmp_min = min;
+// 	i = -1;
+// 	while (++i < max - tmp_min + 1 - cnt_max)
+// 	{
+// 		if (stack->stack_a->number == min)
+// 		{
+// 			ra(stack, 'a');
+// 			min++;
+// 		}
+// 		else
+// 			pb(stack);
+// 	}
+// 	b_sort(stack, stack->size_b, max - cnt_max, min);
+// 	while (cnt_max-- > 0)
+// 		ra(stack, 'a');
+// }
 
 static void	b_sort(t_stack *stack, int size, int max, int min)
 {
@@ -142,12 +247,12 @@ static void	b_sort(t_stack *stack, int size, int max, int min)
 			{
 				pa(stack);
 				min++;
-				if (stack->stack_b->number != max - count && stack->stack_b->number != min && (check_r_rr(*stack, max) == 'r' || check_r_rr(*stack, min) == 'r'))
+				if (stack->stack_b->number != max - count && stack->stack_b->number != min && (check_r_rr(*stack, max - count) == 'r' || check_r_rr(*stack, min) == 'r'))
 					rr(stack);
 				else
 					ra(stack, 'a');
 			}
-			else if (check_r_rr(*stack, max) == 'r' || check_r_rr(*stack, min) == 'r')
+			else if (check_r_rr(*stack, max - count) == 'r' || check_r_rr(*stack, min) == 'r')
 				rb(stack, 'b');
 			else
 				rrb(stack, 'b');
