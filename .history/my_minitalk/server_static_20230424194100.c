03cf50792dcbd053f1aa0e8c79cf92b5cce12789
Ryosuke Nutaba <rnutaba@c1r11s7.42tokyo.jp>
2023-04-24 19:56:41 +0900

    check return

---
+++ .history/my_minitalk/server_static_20230424194100.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server_static.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnutaba <rnutaba@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/18 15:54:27 by nutar             #+#    #+#             */
+/*   Updated: 2023/04/24 19:41:00 by rnutaba          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_minitalk.h"
+
+void	recieve_char(int sig, siginfo_t *info, void *p)
+{
+	static char	c;
+	static int	bit_count;
+
+		if (kill(info->si_pid, SIGUSR1) == -1)
+		{
+			ft_printf("[kill error\n");
+			exit(FAILURE);
+		}
+	if (sig == SIGUSR2)
+		c = c + (1 << bit_count) & 0x000000FF;
+	bit_count++;
+	if (bit_count == 8)
+	{
+		write(1, &c, 1);
+		c = 0;
+		bit_count = 0;
+	}
+	(void)p;
+}
+
+void	server_reciever(void handler(int, siginfo_t *, void *))
+{
+	struct sigaction	act;
+
+	bzero(&act, sizeof(struct sigaction));
+	act.sa_sigaction = handler;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = SA_SIGINFO;
+	sigaddset(&act.sa_mask, SIGUSR1);
+	sigaddset(&act.sa_mask, SIGUSR2);
+	sigaction(SIGUSR1, &act, NULL);
+	sigaction(SIGUSR2, &act, NULL);
+}
+
+int	main(void)
+{
+	ft_printf("PID: %d\n", getpid());
+	server_reciever(recieve_char);
+	while (1)
+		;
+	return (SUCCESS);
+}
